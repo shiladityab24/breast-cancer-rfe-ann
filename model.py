98e0568d6fb07b276d70a448dddc24bb425d3e66
Shiladitya Bose <39332926+shiladityab24@users.noreply.github.com>
2022-03-23 21:54:11 +0530

    Update model.py

--- model.py
+++ model.py
@@ -40,21 +40,21 @@ x_test = scaler.transform(X_test)
 
 
 ##
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
+#import keras
+#from keras.models import Sequential
+#from keras.layers import Dense, Dropout
 
-classifier = Sequential()
-classifier.add(Dense(16, activation='relu', input_dim=15))
-classifier.add(Dropout(rate=0.1))
-classifier.add(Dense(1, activation='sigmoid'))
-classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-classifier.fit(X_train, y_train, batch_size=75, epochs=120)
-y_pred = classifier.predict(X_test)
-y_pred = (y_pred > 0.5)
-# clf_lr = LogisticRegression()
-# clf_lr.fit(x_train, y_train)
-# predictions = clf_lr.predict(x_test)
+#classifier = Sequential()
+#classifier.add(Dense(16, activation='relu', input_dim=15))
+#classifier.add(Dropout(rate=0.1))
+#classifier.add(Dense(1, activation='sigmoid'))
+#classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+#classifier.fit(X_train, y_train, batch_size=75, epochs=120)
+#y_pred = classifier.predict(X_test)
+#y_pred = (y_pred > 0.5)
+clf_lr = LogisticRegression()
+clf_lr.fit(x_train, y_train)
+y_pred = clf_lr.predict(x_test)
 
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
@@ -64,7 +64,7 @@ print("Confusion Matrix : \n\n" , confusion_matrix(y_pred,y_test))
 print("Classification Report : \n\n" , classification_report(y_pred,y_test),"\n")
 
 
-pickle.dump(classifier, open('model.pkl', 'wb'))
+pickle.dump(clf_lr, open('model.pkl', 'wb'))
 pickle.dump(scaler, open('scaler.pkl', 'wb'))
 
 model = pickle.load(open('model.pkl', 'rb'))
